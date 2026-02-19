6f2754727d76c32a887f52462b3e0ebd4493d070
joao5435 <jv6858375@gmail.com>
2026-02-19 20:58:04 -0300

    feat: Header

---
+++ storybook-react/src/components/Header/Header.stories.js
@@ -0,0 +1,14 @@
+import Header from '.'
+
+const meta = {
+  title: 'Components/Header',
+  component: Header,
+}
+
+export default meta
+
+export const Default = {
+  args: {
+    title: 'Meu Header',
+  },
+}
